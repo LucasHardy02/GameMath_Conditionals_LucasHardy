7a44f45731f2ff4f4b05b0f0aa9655e622aba2f3
Lucas Hardy <lucashardywork@gmail.com>
2025-09-15 12:15:28 -0300

    Final Update

--- GameMath_Conditionals_LucasHardy/Program.cs
+++ GameMath_Conditionals_LucasHardy/Program.cs
@@ -11,34 +11,43 @@ namespace GameMath_Conditionals_LucasHardy
     internal class Program
     {
         static int enemyHealth = 100;
-
-        static int playerHealth = 100;
-
-        static string healthStatus = "";
-
-        static WeaponType weapon = WeaponType.fist;
+        
+        static int health = 100;
+        static string healthName = "Health:";
+        static string healthStatus = HealthStatus(health);
+        static string healthStatusName = "Health Status:";
+        static WeaponType weapon = WeaponType.Fist;
+        static string weaponName = "Weapon:";
 
 
         enum WeaponType
         {
-            fist,
-            slingshot,
-            axe,
-            pistol,
-            shotgun,
-            assaultRifle
+            Fist,
+            Slingshot,
+            Axe,
+            Pistol,
+            Shotgun,
+            AssaultRifle
         }
 
         static void ShowHUD()
         {
-            Console.WriteLine("{0,0}{1,10}{2,10}{3,10}", playerHealth, weapon, healthStatus);
+            Console.WriteLine("{0,0}{1,10}{2,20}", healthName, weaponName, healthStatusName);
+            Console.WriteLine("{0,0}{1,10}{2,20}", health, weapon, HealthStatus(health));
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+
+
         }
 
-
+        static int heal(int healthRecovered)
+        {
+            health = health + healthRecovered;
+            return healthRecovered;
+        }
         static void changeWeapon(WeaponType newWeapon)
         {
-            //if weaponEquiped = 0;
-            //dealDamage = 5;
             weapon = newWeapon;
 
 
@@ -49,49 +58,108 @@ namespace GameMath_Conditionals_LucasHardy
             enemyHealth = enemyHealth - WeaponType;
         }
 
-        static void takeDamage(int damageTaken)
+       
+
+
+        
+        static int takeDamage(int damageTaken)
         {
-            playerHealth = playerHealth - damageTaken;
+            health = health - damageTaken;
+            return damageTaken;
         }
         static void Main(string[] args)
         {
 
-            changeWeapon(WeaponType.fist);
-            changeWeapon(WeaponType.slingshot);
-            changeWeapon(WeaponType.axe);
-            changeWeapon(WeaponType.pistol);
-            changeWeapon(WeaponType.shotgun);
-            changeWeapon(WeaponType.assaultRifle);
 
+            changeWeapon(WeaponType.Fist);
+            ShowHUD();
+
+            
+
+            Console.WriteLine("You start your adventure off empty handed, Be careful!");
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("You notice a green silouette approaching...");
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine($"Goblin punches you in the face and runs off dealing {takeDamage(27)} damage");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+           
+
+            Console.WriteLine("The goblin ran away");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            ShowHUD();
+
+            Console.WriteLine("You reach into your pocket for a health potion you stored earlier.");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine($"It heals {heal(27)} health");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            ShowHUD();
+
+            Console.WriteLine("You swap to your Slingshot incase something else comes up on you.");
+            changeWeapon(WeaponType.Slingshot);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            ShowHUD();
+
+
+
+
+            changeWeapon(WeaponType.Slingshot);
+            changeWeapon(WeaponType.Axe);
+            changeWeapon(WeaponType.Pistol);
+            changeWeapon(WeaponType.Shotgun);
+            changeWeapon(WeaponType.AssaultRifle);
+
+            
         }
 
-        static void HealthStatus(int health)
-        {
-            if (health == 0.10)
+        static string HealthStatus(int health)
+        { 
+            if (health >= 100)
             {
-                Console.Write("Health is Critically Low");
+                return "Health is Perfect";
             }
-
-            else if (health == 10.50)
-            {
-                Console.Write("Health is Very Low");
-            }
-
-            else if (health == 50.75)
-            {
-                Console.Write("Health is Fair");
-            }
-            else if (health == 75.99)
-            {
-                Console.Write("Health is Good");
-            }
-            else (health == 100)
-            {
-                Console.Write("Health is Perfect");
-            }
-            return;
-       }     
             
+              else if (health <= 99)
+            {
+                return "Health is Good";
+            }
+
+            else if (health <= 75)
+            {
+                return "Health is Fair";
+            }
+
+            else if (health <= 50)
+            {
+                return "Health is Very Low";
+            }
+
+            if (health <= 10)
+            {
+                return "Health is Critically Low";
+            }
+
+            return "Unknown Health Statue";
+       }     
+          
 
 
     }
